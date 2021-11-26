1c80acc2dfc15e25ffb12cbf95c2bc5c47a8d3d8
iggallagher <44699849+iggallagher@users.noreply.github.com>
2021-11-26 15:39:02 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Spectral Embedding',
+    url='https://github.com/iggallagher/Spectral-Embedding',
+    author='Ian Gallagher',
+    author_email='ian.gallagher@bristol.ac.uk',
+    packages=['spectral_embedding'],
+    install_requires=['numpy'],
+    version='0.1',
+    license='MIT',
+    description='Python package for spectral embedding of networks',
+)
