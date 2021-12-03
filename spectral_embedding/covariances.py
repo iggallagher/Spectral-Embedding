d079df10ec29f3942efc9b5deab138c0efb8a6d5
iggallagher <44699849+iggallagher@users.noreply.github.com>
2021-12-03 21:24:20 +0000

    Added covariance functions

--- spectral_embedding/covariances.py
+++ spectral_embedding/covariances.py
@@ -1,6 +1,29 @@
 import numpy as np
 
 
+def gaussian_ellipse(mean, cov):
+    if mean.shape == (1,2):
+        mean = np.array(mean)[0]
+    v, w = np.linalg.eigh(cov)
+    u = w[0] / np.linalg.norm(w[0])
+    rtheta = np.arctan2(u[1], u[0])
+    v = 3. * np.sqrt(2.) * np.sqrt(v)
+    width = v[0]; height = v[1]
+    
+    R = np.array([
+        [np.cos(rtheta), -np.sin(rtheta)],
+        [np.sin(rtheta),  np.cos(rtheta)],
+        ])    
+    theta = np.deg2rad(np.arange(0.0, 360.0, 1.0))
+    x = 0.5 * width * np.cos(theta)
+    y = 0.5 * height * np.sin(theta)    
+    x, y = np.dot(R,np.array([x, y]))
+    x += mean[0]
+    y += mean[1]
+    
+    return [x,y]
+
+
 def SBM_covariance(A, B, Z, pi, d):
     return WSBM_covariance(A, B, B*(1-B), Z, pi, d):
 
