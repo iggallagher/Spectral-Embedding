50ea9846da76bb8133d21e2d7b10c61ea70f3cd3
iggallagher <44699849+iggallagher@users.noreply.github.com>
2021-12-06 11:09:44 +0000

    Added dynamic WSBM distributions

--- spectral_embedding/distribution.py
+++ spectral_embedding/distribution.py
@@ -7,22 +7,20 @@ def SBM_distbn(A, B, Z, pi, d):
 
 def WSBM_distbn(A, B, C, Z, pi, d):
     P = B[Z,:][:,Z]
-    K = B.shape[0]
+    K = len(pi)
             
     # Spectral embeddings
     UA, SA, VAt = np.linalg.svd(A); VA = VAt.T
     UB, SB, VBt = np.linalg.svd(B); VB = VBt.T
     UP, SP, VPt = np.linalg.svd(P); VP = VPt.T
 
-    XA = UA[:,0:d] @ np.diag(np.sqrt(SA[0:d]))
     XB = UB[:,0:d] @ np.diag(np.sqrt(SB[0:d]))
     XP = UP[:,0:d] @ np.diag(np.sqrt(SP[0:d]))
-
     XZ = XB[Z,:]
     
     # Find spectral embedding map to latent positions
     UW, _, VWt = np.linalg.svd(UP[:,0:d].T @ UA[:,0:d] + VP[:,0:d].T @ VA[:,0:d])
-    W = UW.dot(VWt)
+    W = UW @ VWt
     L = np.linalg.inv(XZ.T @ XZ) @ XZ.T @ XP
     
     X = XB @ L @ W
@@ -31,21 +29,73 @@ def WSBM_distbn(A, B, C, Z, pi, d):
     XBinv = np.linalg.pinv(XB)
     Lambda = XBinv @ B @ XBinv.T
         
-    Sigmas = np.zeros((K,d,d))
+    Sigma = np.zeros((K,d,d))
     for i in range(K):
         for j in range(K):
-            Sigmas[i] += pi[j]*C[i,j]*np.outer(XB[j],XB[j])
+            Sigma[i] += pi[j]*C[i,j]*np.outer(XB[j],XB[j])
         
     Delta = np.zeros((d,d))
     for i in range(K):
         Delta += pi[i]*np.outer(XB[i],XB[i])
    
     D = np.linalg.inv(Lambda @ Delta @ Lambda.T) @ Lambda
-    SigmaXs = np.zeros((K,d,d))
+    SigmaX = np.zeros((K,d,d))
     for i in range(K):
-        SigmaXs[i] = W.T @ L.T @ D @ Sigmas[i] @ D.T @ L @ W
+        SigmaX[i] = W.T @ L.T @ D @ Sigma[i] @ D.T @ L @ W
    
-    return (X, SigmaXs)
+    return (X, SigmaX)
+
+
+def SBM_dynamic_distbn(As, Bs, Z, pi, d):
+    return WSBM_dynamic_distbn(As, Bs, Bs*(1-Bs), Z, pi, d)
+
+
+def WSBM_dynamic_distbn(As, Bs, Cs, Z, pi, d):
+    T = As.shape[0]
+    K = len(pi)
+    
+    A = np.block([A for A in As])
+    B = np.block([B for B in Bs])
+    P = np.block([B[Z,:][:,Z] for B in Bs])
+    
+    # Spectral embeddings
+    UA, SA, VAt = np.linalg.svd(A); VA = VAt.T
+    UB, SB, VBt = np.linalg.svd(B); VB = VBt.T
+    UP, SP, VPt = np.linalg.svd(P); VP = VPt.T
+
+    XB = UB[:,0:d] @ np.diag(np.sqrt(SB[0:d]))
+    XP = UP[:,0:d] @ np.diag(np.sqrt(SP[0:d]))
+    XZ = XB[Z,:]
+        
+    # Map spectral embeddings to latent positions
+    UW, _, VWt = np.linalg.svd(UP[:,0:d].T @ UA[:,0:d] + VP[:,0:d].T @ VA[:,0:d])
+    W = UW @ VWt
+    L = np.linalg.inv(XZ.T @ XZ) @ XZ.T @ XP
+    R = np.linalg.inv(L.T) @ W
+    
+    # Covariance matrices    
+    Sigmas = np.zeros((T,K,d,d))
+    for t in range(T):
+        for i in range(K):
+            for j in range(K):
+                Sigmas[t,i] += pi[j]*Cs[t,i,j]*np.outer(XB[j],XB[j])
+          
+    Delta = np.zeros((d,d))
+    for i in range(K):
+        Delta += pi[i]*np.outer(XB[i],XB[i])
+    DeltaInv = np.linalg.inv(Delta)
+
+    Ys = np.zeros((T,d,d))
+    
+    for t in range(T):    
+        Ys[t] = VB[t*d:(t+1)*d,0:d] @ np.diag(np.sqrt(SB[0:d])) @ R       
+    
+    SigmaYs = np.zeros((T,K,d,d))
+    for t in range(T):
+        for i in range(K):
+            SigmaYs[t,i] = R.T @ DeltaInv @ Sigmas[t,i] @ DeltaInv @ R
+    
+    return (Ys, SigmaYs)
 
 
 def gaussian_ellipse(mean, cov):
