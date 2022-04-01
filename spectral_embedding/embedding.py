8e1c58783d63bc7cdd15de17d81480ba85e1f319
iggallagher <44699849+iggallagher@users.noreply.github.com>
2022-04-01 18:57:14 +0100

    Changed order of sparse matrix embedding

--- spectral_embedding/embedding.py
+++ spectral_embedding/embedding.py
@@ -7,33 +7,36 @@ import scipy.sparse as sparse
 def left_embed(A, d):
     if sparse.issparse(A):
         UA, SA, VAt = sparse.linalg.svds(A, d)
+        XA = UA[:,d:0] @ np.diag(np.sqrt(SA[d:0]))
     else:
         UA, SA, VAt = np.linalg.svd(A)
-        
-    XA = UA[:,0:d] @ np.diag(np.sqrt(SA[0:d]))  
+        XA = UA[:,0:d] @ np.diag(np.sqrt(SA[0:d]))  
     return XA
 
 
 def right_embed(A, d):
     if sparse.issparse(A):
         UA, SA, VAt = sparse.linalg.svds(A, d)
+        VA = VAt.T
+        YA = VA[:,d:0] @ np.diag(np.sqrt(SA[d:0])) 
     else:
         UA, SA, VAt = np.linalg.svd(A)
-        
-    VA = VAt.T
-    YA = VA[:,0:d] @ np.diag(np.sqrt(SA[0:d]))  
+        VA = VAt.T
+        YA = VA[:,0:d] @ np.diag(np.sqrt(SA[0:d]))  
     return YA
 
 
 def both_embed(A, d):
     if sparse.issparse(A):
         UA, SA, VAt = sparse.linalg.svds(A, d)
+        VA = VAt.T
+        XA = UA[:,d:0] @ np.diag(np.sqrt(SA[d:0]))
+        YA = VA[:,d:0] @ np.diag(np.sqrt(SA[d:0])) 
     else:
         UA, SA, VAt = np.linalg.svd(A)
-        
-    VA = VAt.T
-    XA = UA[:,0:d] @ np.diag(np.sqrt(SA[0:d]))
-    YA = VA[:,0:d] @ np.diag(np.sqrt(SA[0:d]))  
+        VA = VAt.T
+        XA = UA[:,0:d] @ np.diag(np.sqrt(SA[0:d]))
+        YA = VA[:,0:d] @ np.diag(np.sqrt(SA[0:d]))  
     return (XA, YA)
 
 
