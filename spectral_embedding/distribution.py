50cac40d97c1a42846e207f30a4c06a003bb0b7d
iggallagher <44699849+iggallagher@users.noreply.github.com>
2022-05-04 18:54:49 +0100

    Added MMSBM distributions

--- spectral_embedding/distribution.py
+++ spectral_embedding/distribution.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.special import gamma
 
 
 def SBM_distbn(A, B, Z, pi, d):
@@ -37,6 +38,73 @@ def WSBM_distbn(A, B, C, Z, pi, d):
     Delta = np.zeros((d,d))
     for i in range(K):
         Delta += pi[i]*np.outer(XB[i],XB[i])
+   
+    D = np.linalg.inv(Lambda @ Delta @ Lambda.T) @ Lambda
+    SigmaX = np.zeros((K,d,d))
+    for i in range(K):
+        SigmaX[i] = W.T @ L.T @ D @ Sigma[i] @ D.T @ L @ W
+   
+    return (X, SigmaX)
+
+
+def dirichlet_moment(alpha, beta):
+    alpha0 = np.sum(alpha)
+    beta0  = np.sum(beta)
+    K = len(alpha)
+    
+    moment = gamma(alpha0) / gamma(alpha0 + beta0)
+    for k in range(K):
+        moment *= gamma(alpha[k] + beta[k]) / gamma(alpha[k])
+        
+    return moment
+
+
+def MMSBM_distbn(A, B, C, Z, alpha, d):
+    return WMMSBM_distbn(A, B, B*(1-B), Z, alpha, d)
+
+
+def WMMSBM_distbn(A, B, C, Z, alpha, d):
+    P = Z @ B @ Z.T
+    K = len(alpha)
+            
+    # Spectral embeddings
+    UA, SA, VAt = np.linalg.svd(A); VA = VAt.T
+    UB, SB, VBt = np.linalg.svd(B); VB = VBt.T
+    UP, SP, VPt = np.linalg.svd(P); VP = VPt.T
+
+    XB = UB[:,0:d] @ np.diag(np.sqrt(SB[0:d]))
+    XP = UP[:,0:d] @ np.diag(np.sqrt(SP[0:d]))
+    XZ = Z @ XB
+    
+    # Find spectral embedding map to latent positions
+    UW, _, VWt = np.linalg.svd(UP[:,0:d].T @ UA[:,0:d] + VP[:,0:d].T @ VA[:,0:d])
+    W = UW @ VWt
+    L = np.linalg.inv(XZ.T @ XZ) @ XZ.T @ XP
+    
+    X = XB @ L @ W
+    
+    # Covariance matrices
+    XBinv = np.linalg.pinv(XB)
+    Lambda = XBinv @ B @ XBinv.T
+    
+    Sigma = np.zeros((K,d,d))
+    for i in range(K):
+        for j in range(K):
+            for k in range(K):
+                for l in range(K):
+                    beta_C = np.zeros((K))
+                    beta_C[j] += 1; beta_C[k] += 1; beta_C[l] += 1
+                    beta_B = np.zeros((K))
+                    beta_B[j] += 2; beta_B[k] += 1; beta_B[l] += 1
+                    Sigma[i] += (dirichlet_moment(alpha, beta_C)*C[i,j] - 
+                                 dirichlet_moment(alpha, beta_B)*B[i,j]**2) * np.outer(XB[k],XB[l])
+        
+    Delta = np.zeros((d,d))
+    for i in range(K):
+        for j in range(K):
+            beta = np.zeros((K))
+            beta[i] += 1; beta[j] += 1
+            Delta += dirichlet_moment(alpha, beta) * np.outer(XB[i],XB[j])
    
     D = np.linalg.inv(Lambda @ Delta @ Lambda.T) @ Lambda
     SigmaX = np.zeros((K,d,d))
